7bce134bd3e54ad5c696fb3901e836e071240aec
RossT <rthomson412@gmail.com>
2022-04-20 13:11:21 -0400

    ahh

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-console.log(profileDataArgs);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
-console.log(name, github);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer.prompt([
 
-const pageHTML = generatePage(name, github);
+{
+  type: 'input',
+  name: 'name',
+  message: 'What is your name?'
+}
+])
+.then(answers=>console.log(answers));
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
